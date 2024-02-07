e37847f1b3d85ff0a3ede3bce45620c5a70e58de
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-02-07 07:35:34 -0300

    feat: creating create-poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,27 +1,7 @@
-import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client/extension'
-import { z } from 'zod'
+import fastify from "fastify";
 
-const app = fastify()
+const app = fastify();
 
-const prisma = new PrismaClient()
-
-app.post('polls', async (request, reply) => {
-    const createPollBody = z.object({
-      title: z.string()
-    })
-
-    const { title } = createPollBody.parse(request.body)
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        }
-    })
-
-    return reply.status(201).send({ pollId: poll.id})
-})
-
-app.listen({ port: 3333}).then(() => {
-    console.log("HTTP server running")
-})
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running");
+});
